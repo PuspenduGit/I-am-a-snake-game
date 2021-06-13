4702abde28b540ba4be77690b11ec6ddd8cddece
Puspendu Das <77235824+PuspenduGit@users.noreply.github.com>
2021-06-13 23:39:29 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -103,7 +103,7 @@ function gameEngine(){
 
 
 // Main logic starts here
-musicSound.play();
+ musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
     hiscoreval = 0;
